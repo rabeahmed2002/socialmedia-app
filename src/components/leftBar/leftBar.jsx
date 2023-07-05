7274d649a0dba08a5e4c40205aba21196afe1161
rabeahmed2002 <mrabe2024@gmail.com>
2023-07-05 17:46:10 +0500

    navbar complete

--- src/components/leftBar/leftBar.jsx
+++ src/components/leftBar/leftBar.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const leftBar = () => {
   return (
     <div className='leftBar'>
-      
+        <div className='container'>
+          <div className='menu'>
+            <div className='user'>
+              
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
