c7f476342cfab9c5f6a1c379a23198100e39229c
rabeahmed2002 <mrabe2024@gmail.com>
2023-07-06 23:10:33 +0500

    stories and posts created

---
+++ src/components/Posts/posts.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './posts.scss'
+
+const posts = () => {
+  return (
+    <div className='posts'>
+      
+    </div>
+  )
+}
+
+export default posts
---
+++ src/components/Stories/stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './stories.scss'
+
+const stories = () => {
+  return (
+    <div className='stories'>
+      
+    </div>
+  )
+}
+
+export default stories
